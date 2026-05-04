4b71a5bdc8eaa2020a5b9ae0a6db9e8a182c8e1a
jnsousa777 <juansousa3011@gmail.com>
2026-05-04 18:41:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// opcional
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
